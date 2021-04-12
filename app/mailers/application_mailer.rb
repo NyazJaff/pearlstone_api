353e77c2acaffb0a95c508cc416df32d1ec93da9
N Jaff <n.jaff@livelinktechnology.net>
2021-04-12 14:45:03 +0100

    Adding sender and support email to saving report

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'Pearlstone Energy Ltd <info@pearlstoneenergy.com>'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
 require 's3_storage'
 
 class UserMailer < ApplicationMailer
-  default from: "Pearlstone Energy Ltd <nyaz.k.jaff@gmail.com>"
+  default from: "Pearlstone Energy Ltd <#{ENV['DEFAULT_EMAIL_FROM']}>"
 
   def saving_estimate_report_email(file_key, user)
     @user = user
     S3Storage.tmp_file(file_key) do |attachment|
       attachments[file_key.split('/').last] = File.read(attachment.path)
-      mail(to: user.email, subject: 'Pearlstone - Saving Estimate - ' + Time.now.strftime("%d-%m-%Y").to_s)
+      mail(to: user.email, subject: 'Pearlstone - Saving Estimate - ' + Time.now.strftime("%d-%m-%Y").to_s,
+           :cc => ENV['SUPPORT_EMAIL']
+      )
     end
   end
 end
